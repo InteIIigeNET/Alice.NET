748a40a8d81b716626aca49b32e8c65c463f201f
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2018-07-28 23:10:14 +0300

    ...

--- Alice.NET/Extensions/AliceExtensions.cs
+++ Alice.NET/Extensions/AliceExtensions.cs
@@ -4,19 +4,19 @@ namespace Alice.Extensions
 {
 	public static class AliceExtensions
 	{
-		public static AliceResponse Reply(this AliceRequest req,
-											   string text,
-											   bool endSession = false,
-											   Button[] buttons = null) 
-		=> new AliceResponse
-		{
-			Response = new Response
+		public static AliceResponse Reply(this AliceRequest request,
+			string text,
+			bool endSession = false,
+			Button[] buttons = null)
+			=> new AliceResponse
 			{
-				Text = text,
-				Tts = text,
-				EndSession = endSession
-			},
-			Session = req.Session
-		};
+				Response = new Response
+				{
+					Text = text,
+					Tts = text,
+					EndSession = endSession
+				},
+				Session = request.Session
+			};
 	}
 }
