2f126726458c30a75ee86c6002393de3f8319795
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2018-08-06 01:24:05 +0300

    yet another fixes

--- Alice.NET/Models/Cards/BigImageCard.cs
+++ Alice.NET/Models/Cards/BigImageCard.cs
@@ -1,4 +1,5 @@
 using Alice.Models.Buttons;
+using Newtonsoft.Json;
 
 namespace Alice.Models.Cards
 {
@@ -13,21 +14,25 @@ namespace Alice.Models.Cards
 		/// <summary>
 		/// Идентификатор изображения, который возвращается в ответ на запрос загрузки
 		/// </summary>
+		[JsonProperty("image_id")]
 		public string ImageId { get; set; }
 
 		/// <summary>
 		/// Заголовок для изображения
 		/// </summary>
+		[JsonProperty("title")]
 		public string Title { get; set; }
 
 		/// <summary>
 		/// Описание изображения
 		/// </summary>
+		[JsonProperty("description")]
 		public string Description { get; set; }
 
 		/// <summary>
 		/// Свойства изображения, на которое можно нажать
 		/// </summary>
+		[JsonProperty("button")]
 		public Button Button { get; set; }
 	}
 }
