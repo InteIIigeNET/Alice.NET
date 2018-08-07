2ec9513e404ef9b0bed0e72aab53e109cdcf2d5f
MaxVortman <maka.abort@gmail.com>
2018-08-07 22:54:44 +0300

    Must have native ioc func wraped.

---
+++ Alice.NET.Attributes/RequestHandlerAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alice.NET.Attributes
+{
+    public class RequestHandlerAttribute : Attribute
+    {
+        public RequestHandlerAttribute(string url)
+        {
+            Url = url;
+        }
+
+        public string Url { get; }
+    }
+}
---
+++ Alice.NET.Attributes/RequestListenerAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alice.NET.Attributes
+{
+    public class RequestListenerAttribute : Attribute
+    {
+        public RequestListenerAttribute(string url)
+        {
+            Url = url;
+        }
+
+        public string Url { get; }
+    }
+}
