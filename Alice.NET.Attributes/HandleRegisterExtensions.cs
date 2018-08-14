b2279bed9e2454883ba6796c9cceb08090f83505
MaxVortman <maka.abort@gmail.com>
2018-08-14 08:46:05 +0300

    Small refactoring.

--- Alice.NET.Attributes/HandleRegisterExtensions.cs
+++ Alice.NET.Attributes/HandleRegisterExtensions.cs
@@ -17,41 +17,65 @@ namespace Alice.NET.Attributes
                 throw new ArgumentException();
             foreach (var handlerPair in handlers)
             {
-                var url = handlerPair.Key;
-                var handler = handlerPair.Value;
-                var handlerListeners = listeners.ContainsKey(url) ? listeners[url] : null;
-                handleRegister.EnrolHandler(url, handler, handlerListeners?.ToArray());
+                EnrolHandlerAndListeners(handleRegister, listeners, handlerPair);
+            }
+        }
+
+        private static void EnrolHandlerAndListeners(HandleRegister handleRegister, Dictionary<string, List<IAliceListener>> listeners, KeyValuePair<string, IAliceHandler> handlerPair)
+        {
+            var url = handlerPair.Key;
+            var handler = handlerPair.Value;
+            if (listeners.ContainsKey(url))
+            {
+                handleRegister.EnrolHandler(url, handler, listeners[url].ToArray());
+            }
+            else
+            {
+                handleRegister.EnrolHandler(url, handler);
             }
         }
 
         private static void GetHandlersAndListeners(Dictionary<string, IAliceHandler> handlers, Dictionary<string, List<IAliceListener>> listeners)
         {
-            var assemblies = Assembly.GetCallingAssembly().GetApplicationAssemblies();
-            var types = assemblies.SelectMany(item => item.GetExportedTypes());
-            foreach (var type in types)
+            foreach (var type in GetTypes())
             {
-                if (type.TryGetAttribute(out RequestHandlerAttribute requestHandlerAttribute)
-                    && type.IsInheritInterface(nameof(IAliceHandler)))
+                if (TryGetAttributOnInheritInterfaceType<RequestHandlerAttribute, IAliceHandler>(type, out var requestHandlerAttribute))
                 {
                     handlers.Add(requestHandlerAttribute.Url, type.CreateObject<IAliceHandler>());
-                    continue;
                 }
-                if (type.TryGetAttribute(out RequestListenerAttribute requestListenerAttribute)
-                    && type.IsInheritInterface(nameof(IAliceListener)))
+                else if (TryGetAttributOnInheritInterfaceType<RequestListenerAttribute, IAliceListener>(type, out var requestListenerAttribute))
                 {
-                    var url = requestListenerAttribute.Url;
-                    var obj = type.CreateObject<IAliceListener>();
-                    if (listeners.ContainsKey(url))
-                    {
-                        listeners[url].Add(obj);
-                    }
-                    else
-                    {
-                        listeners.Add(url, new List<IAliceListener> { obj });
-                    }
-                    continue;
+                    FillListeners(listeners, type, requestListenerAttribute);
                 }
             }
+        }
+
+        private static void FillListeners(Dictionary<string, List<IAliceListener>> listeners, Type type, RequestListenerAttribute requestListenerAttribute)
+        {
+            var url = requestListenerAttribute.Url;
+            var obj = type.CreateObject<IAliceListener>();
+            if (listeners.ContainsKey(url))
+            {
+                listeners[url].Add(obj);
+            }
+            else
+            {
+                listeners.Add(url, new List<IAliceListener> { obj });
+            }
+        }
+
+        private static bool TryGetAttributOnInheritInterfaceType<TAttribute, TInterface>(Type type, out TAttribute requestHandlerAttribute)
+            where TAttribute : Attribute
+        {
+            return type.TryGetAttribute(out requestHandlerAttribute)
+                                && type.IsInheritInterface(nameof(TInterface));
+        }
+
+        private static IEnumerable<Type> GetTypes()
+        {
+            var assemblies = Assembly.GetCallingAssembly().GetApplicationAssemblies();
+            var types = assemblies.SelectMany(item => item.GetExportedTypes());
+            return types;
         }
     }
 }
